dff282d5b042f2e338bbcb196c5fa43d43d6f02e
artanvir19 <tanvirahmed4989@gmail.com>
2024-05-31 20:50:35 +0600

    1st commit

--- python_homework/01.print_info.py
+++ python_homework/01.print_info.py
@@ -3,3 +3,5 @@ id=int(input("Enter your ID: "))
 dept=input("Enter your Department: ")
 batch=input("Enter your batch: ")
 print("name:",name,"\nID:",id,"\nDepartment:",dept,"\nBatch:",batch)
+
+#####################################
